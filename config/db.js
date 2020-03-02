4a140bbb1d35223b0b03ce214785877badb200b7
Miguel Cabrera <miguel.cabrera.1095@gmail.com>
2020-03-02 14:26:24 -0600

    Connect to DB

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+// Connect to the Mongo DB
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI || db, {
+      useNewUrlParser: true
+    });
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
